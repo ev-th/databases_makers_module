263663294262e2d0ef8da503221c5a8f93b44176
Evan Thomas <evanthomas0292@gmail.com>
2023-04-25 11:35:48 +0100

    Add music_library artists from the instructional video

--- 02_challenges/music_library/app.rb
+++ 02_challenges/music_library/app.rb
@@ -1,10 +1,7 @@
 require_relative "lib/database_connection"
+require_relative "lib/artist_repository"
 
 DatabaseConnection.connect("music_library")
 
-sql = "SELECT id, title FROM albums;"
-result = DatabaseConnection.exec_params(sql, [])
-
-result.each do |record|
-  p record
-end
+artist_repository = ArtistRepository.new
+artist_repository.all.each{ |artist| p artist }
