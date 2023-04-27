aac0f8870fda7e938d7550af4e946657721ee247
Evan Thomas <evanthomas0292@gmail.com>
2023-04-27 10:52:24 +0100

    Setup student_repository_2 file structure

---
+++ 02_challenges/student_directory_2/spec/seeds_cohorts.sql
@@ -0,0 +1,15 @@
+-- (file: spec/seeds_albums.sql)
+
+-- Write your SQL seed here. 
+
+-- First, you'd need to truncate the table - this is so our table is emptied between each test run,
+-- so we can start with a fresh state.
+-- (RESTART IDENTITY resets the primary key)
+
+TRUNCATE TABLE cohorts RESTART IDENTITY; -- replace with your own table name.
+
+-- Below this line there should only be `INSERT` statements.
+-- Replace these statements with your own seed data.
+
+INSERT INTO cohorts (name, starting_date) VALUES ('cohort_1', "2023-03-15");
+INSERT INTO cohorts (name, starting_date) VALUES ('cohort_2', "2023-04-21");
