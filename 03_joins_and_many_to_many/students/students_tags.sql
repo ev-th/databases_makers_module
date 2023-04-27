d45de4a7e22633f38abdd7039766b3ea70a47992
Evan Thomas <evanthomas0292@gmail.com>
2023-04-27 18:13:19 +0100

    Design students_tags many to many database

---
+++ 03_joins_and_many_to_many/students/students_tags.sql
@@ -0,0 +1,24 @@
+-- file: students_tags.sql
+
+-- Replace the table name, columm names and types.
+
+-- Create the first table.
+CREATE TABLE students (
+  id SERIAL PRIMARY KEY,
+  name text
+);
+
+-- Create the second table.
+CREATE TABLE tags (
+  id SERIAL PRIMARY KEY,
+  content text
+);
+
+-- Create the join table.
+CREATE TABLE students_tags (
+  student_id int,
+  tag_id int,
+  constraint fk_student foreign key(student_id) references students(id) on delete cascade,
+  constraint fk_tag foreign key(tag_id) references tags(id) on delete cascade,
+  PRIMARY KEY (student_id, tag_id)
+);
